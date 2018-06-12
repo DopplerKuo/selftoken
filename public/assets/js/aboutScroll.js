143f4276263e32e6c47ffb230816dba6e71eac28
DopplerKuo <doppler.kuo@gmail.com>
2018-06-13 04:08:13 +0800

    Finished 40%

--- public/assets/js/aboutScroll.js
+++ public/assets/js/aboutScroll.js
@@ -1,14 +1,20 @@
-$(function(){
-	$(window).bind('scroll resize', function(){
-　　var $this = $(this);
-　　var $this_Top = $this.scrollTop();
-   var $barFixedHeight = $('.sec1').height() - 140;
-　　if($this_Top < $barFixedHeight){
-　　　 $('nav').removeClass('nav-small');
-　　　}
-　　else if($this_Top > $barFixedHeight){
-　　　 $('nav').addClass('nav-small')
-			$('#index .sec2').addClass('animate')
-　　　}
-　　}).scroll();
+$(function() {
+$(window).bind('scroll resize', function() {
+	var $this = $(this);
+	var $this_Top = $this.scrollTop();
+	var windowHeight = $this.height();
+	var pageHeigh = $('header').height() + 
+									$('.sec2').height() +
+									$('.sec3').height() +
+									$('.sec4').height() +
+									$('.sec5').height() +
+									$('.sec6').height();
+
+
+	var colorfulHeigh = $('.color-bg').height();
+	var moveColorful = (colorfulHeigh - windowHeight) * ($this_Top / ( pageHeigh - windowHeight ));
+	console.log(moveColorful)
+	$('.color-bg').css('transform', 'translateY(-'+ moveColorful +'px)');
+	
+	}).scroll();
 })
